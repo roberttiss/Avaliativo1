5c67a96b08072c99bfe7a4ba4de48c526882c993
gabriel roberts <gabrielrobertssoares@gmail.com>
2023-09-04 09:53:41 -0300

    Add files via upload

---
+++ ProjetoDado/Main.java
@@ -0,0 +1,14 @@
+package ProjetoDado;
+
+public class Main {
+    public static void main(String[] args) {
+        Dado dado;
+
+        for(int i = 0; i != 3; i++){
+            dado = new Dado();
+            dado.imprmir();
+        }
+
+    }
+    
+}
